601ebcba09637ba1d6eec3a7bbda10dc74ecca0c
juan267 <j.gomez267@gmail.com>
2017-11-24 09:51:19 -0500

    more notes

--- challenges/3-Dices/application.js
+++ challenges/3-Dices/application.js
@@ -1,17 +1,68 @@
-$(document).ready(function() {  // Codigo Procedural Procedimental --> OOP
-  // Event Listener
-  if (false) {
-    $('#roller button.add').on('click', function() { // Controlador
-      $('.dice').append('<div class="die">0</div>') // Vista
+// console.log('hola')
+
+//  $(document).ready(function() {  // Codigo Procedural Procedimental --> OOP
+//   // Event Listener
+//   $('#roller button.add').on('click', function() { // Controlador
+//     $('.dice').append('<div class="die">0</div>') // Vista
+//   })
+
+//   // Event listener
+//   $('#roller button.roll').on('click', function() { // Controlador
+//     $('.die').each(function(index, dieHtml) { // Controlador
+//       var value = Math.floor((Math.random()*6)+1) // Model
+//       $(dieHtml).text(value) // Vista
+//     })
+//   })
+// })
+
+$(document).ready(function(){
+
+  var Controller = function(view, model) {
+    this.view = view
+    this.model = model
+  }
+
+  Controller.prototype.bindEvents = function() {
+    console.log(this)
+    $('#hola').on('click', this.view.paintDice)
+    $('#roller button.add').on('click', this.view.paintDice)
+    $('#roller button.roll').on('click', this.rollAllDices.bind(this))
+  }
+
+  Controller.prototype.run = function() {
+    console.log('hola')
+    this.bindEvents()
+    this.view.paintDice()
+  }
+
+  Controller.prototype.rollAllDices = function() {
+    var controller = this
+    $('.die').each(function(index, dieHtml) { // Controlador
+      controller.model.roll()
+      controller.view.rollAllDices(dieHtml, controller.model.value)
     })
   }
 
-  // Event listener
-  $('#roller button.roll').on('click', function() { // Controlador
-    $('.die').each(function(index, dieHtml) { // Controlador
-      var value = Math.floor((Math.random()*6)+1) // Model
-      $(dieHtml).text(value) // Vista
-    })
-  })
-})
+  var Model = function() {
+    this.value = 0
+  }
 
+  Model.prototype.roll = function() {
+    this.value = Math.floor((Math.random()*6)+1) // Model
+  }
+
+  var View = function() {
+
+  }
+
+  View.prototype.paintDice = function() {
+    console.log(this)
+    $('.dice').append('<div class="die">0</div>')
+  }
+
+  View.prototype.rollAllDices =
+  var view = new View()
+  var model = new Model()
+  var game = new Controller(view, model)
+  game.run()
+})
